ba91921fb9021648a4a1994cdac77c220287c2d8
Jan Bureš <jan.bures@live.com>
2023-03-03 15:52:30 +0100

    Experimental SpaceWarp 0.4 support

--- StickyOrbitMarkersProject/StickyOrbitMarkers/StickyOrbitMarkersMod.cs
+++ StickyOrbitMarkersProject/StickyOrbitMarkers/StickyOrbitMarkersMod.cs
@@ -1,13 +1,22 @@
+using BepInEx;
+using HarmonyLib;
+using SpaceWarp;
 using SpaceWarp.API.Mods;
 
 namespace StickyOrbitMarkers
 {
-    [MainMod]
-     public class StickyOrbitMarkersMod: Mod
+    [BepInPlugin(StickyOrbitMarkersMod.ModGuid, StickyOrbitMarkersMod.ModName, StickyOrbitMarkersMod.ModVer)]
+    [BepInDependency(SpaceWarpPlugin.ModGuid, SpaceWarpPlugin.ModVer)]
+    public class StickyOrbitMarkersMod : BaseSpaceWarpPlugin
     {
+        public const string ModGuid = "com.munix.StickyOrbitMarkers";
+        public const string ModName = "Sticky Orbit Markers";
+        public const string ModVer = "0.3.0";
+
         public override void OnInitialized()
         {
-            Logger.Info("Sticky Orbit Markers mod is initialized");
+            Harmony.CreateAndPatchAll(typeof(StickyOrbitMarkersMod).Assembly, StickyOrbitMarkersMod.ModGuid);
+            Logger.LogInfo("Sticky Orbit Markers mod is initialized");
         }
     }
 }
